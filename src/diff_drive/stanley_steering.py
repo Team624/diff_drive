eec3303ea754c5a95d2a1af21e84cacae8120d7b
LeonidasVarveropoulos <leonidas@vrhome.net>
2021-03-06 13:00:51 -0600

    new pure pursuit path follower

--- src/diff_drive/stanley_steering.py
+++ src/diff_drive/stanley_steering.py
@@ -10,42 +10,60 @@ import matplotlib.pyplot as plt
 import sys
 import cubic_spline_planner
 from diff_drive.pose import Pose
-from math import pi, sqrt, sin, cos, atan2
+import math
+
+class State:
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0, wheel_base=0.5842):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        self.wheel_base = wheel_base
+        self.rear_x = self.x - ((self.wheel_base / 2) * math.cos(self.yaw))
+        self.rear_y = self.y + ((self.wheel_base / 2) * math.sin(self.yaw))
+
+    def update(self, x, y, yaw, v):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        print("YAW",self.yaw)
+        self.rear_x = self.x - ((self.wheel_base / 2) * math.cos(self.yaw))
+        self.rear_y = self.y + ((self.wheel_base / 2) * math.sin(self.yaw))
+        print(self.rear_x, self.rear_y)
+
+    def calc_distance(self, point_x, point_y):
+        dx = self.rear_x - point_x
+        dy = self.rear_y - point_y
+        return math.hypot(dx, dy)
 
 class StanleySteering:
-    def __init__(self, ax = [0, 1], ay = [0, 1], target_speed = 2.0, k = 0.5, Kp = 1.0, dt = 0.5, L = 0.5842, max_steer = np.radians(30), show_animation = True):
+    def __init__(self, ax = [0, 1], ay = [0, 1], k = 0.1, Kp = 1.0, dt = 0.5, Ka = 0.4, L = 0.5842, look_ahead = 0.1):
         self.k = k  # control gain
         self.Kp = Kp  # speed proportional gain
-        self.dt = dt  # [s] time difference
-        self.L = L  # [m] Wheel base of vehicle
-        self.max_steer = max_steer  # [rad] max steering angle
+        self.Ka = Ka
+        self.wheel_base = L  # [m] Wheel base of vehicle
 
-        self.show_animation = show_animation
+        self.look_ahead = look_ahead
 
         #  target course
-        self.ax = ax
-        self.ay = ay
+        self.cx = ax
+        self.cy = ay
 
-        self.cx, self.cy, self.cyaw, ck, s = cubic_spline_planner.calc_spline_course(
-            self.ax, self.ay, ds=0.1)
+        # self.cx, self.cy, self.cyaw, ck, s = cubic_spline_planner.calc_spline_course(
+        #     self.ax, self.ay, ds=0.1)
 
-        self.target_speed = target_speed  # [m/s]
+        self.target_course = None
 
         # Initial state
-        self.state = Pose()
+        self.state = State()
 
         self.last_idx = len(self.cx) - 1
-        self.time = 0.0
-        self.x = [self.state.x]
-        self.y = [self.state.y]
-        self.theta = [self.state.theta]
-        self.v = [self.state.xVel]
-        self.t = [0.0]
+        
         self.target_idx = 0
 
-        print("Index", self.last_idx)
-
-        self.is_at_goal = True
+        self.is_at_goal = False
         self.direction = 1
 
         # Parameters
@@ -65,42 +83,26 @@ class StanleySteering:
         self.within_linear_tolerance = False
         self.within_angular_tolerance = False
 
-    def set_path(self, pose, ax = [0,1], ay = [0,1], target_speed = 2.0, L = 0.5842, max_steer = np.radians(30), show_animation = True):
-        self.L = L  # [m] Wheel base of vehicle
-        self.max_steer = max_steer  # [rad] max steering angle
-
-        self.show_animation = show_animation
-
-        self.state.x = pose.x 
-        self.state.y = pose.y 
-        self.state.theta = pose.theta
+    def set_path(self, pose, vel, ax = [0,1], ay = [0,1]):
+        self.state.update(pose.x,pose.y,pose.theta,vel)
 
         #  target course
-        self.ax = ax
-        self.ay = ay
-
-        self.cx, self.cy, self.cyaw, ck, s = cubic_spline_planner.calc_spline_course(
-            self.ax, self.ay, ds=0.1)
+        self.cx = ax
+        self.cy = ay
 
         # self.cx.append(self.cx[len(self.cx)-1])
         # self.cy.append(self.cy[len(self.cy)-1])
-        # self.cyaw.append(self.cyaw[len(self.cyaw)-1])
-
-        self.target_speed = target_speed  # [m/s]
+        # self.cyaw.append(self.cyaw[len(self.cyaw)-1]
 
         self.last_idx = len(self.cx) - 1
 
-        self.time = 0.0
-        self.x = [self.state.x]
-        self.y = [self.state.y]
-        self.theta = [self.state.theta]
-        self.v = [self.state.xVel]
-        self.t = [0.0]
-        self.target_idx, _ = self.calc_target_index()
-        print("target Index", self.target_idx, "Pose", self.state.x, self.state.y)
+        #  target course
+        self.target_course = TargetCourse(self.cx, self.cy)
+        self.target_idx, _ = self.target_course.search_target_index(self.state, self.k, self.Ka, self.look_ahead)
     # Parameters
-    def set_constants(self, Kp, k, blank):
+    def set_constants(self, Kp, k, Ka):
         self.Kp = Kp
+        self.Ka = Ka
         self.k = k
 
     def set_max_linear_speed(self, speed):
@@ -142,7 +144,7 @@ class StanleySteering:
             self.direction = 1
         else:
             self.direction = -1
-            self.target_speed = -self.target_speed
+            self.max_linear_speed = -self.max_linear_speed
 
     def set_end_of_path_stop(self, end_of_path_stop):
         self.end_of_path_stop = end_of_path_stop
@@ -159,7 +161,7 @@ class StanleySteering:
     def get_goal_distance(self):
         diffX = self.state.x - self.cx[len(self.cx) -1]
         diffY = self.state.y - self.cy[len(self.cy) -1]
-        return sqrt(diffX*diffX + diffY*diffY)
+        return math.sqrt(diffX*diffX + diffY*diffY)
 
     def at_goal(self):
         return self.is_at_goal
@@ -171,35 +173,9 @@ class StanleySteering:
         :param current: (float)
         :return: (float)
         """
+
+        print(target,current,self.Kp)
         return self.Kp * (target - current)
-
-
-    def stanley_control(self):
-        """
-        Stanley steering control.
-        :param state: (State object)
-        :param cx: ([float])
-        :param cy: ([float])
-        :param cyaw: ([float])
-        :param last_target_idx: (int)
-        :return: (float, int)
-        """
-        current_target_idx, error_front_axle = self.calc_target_index()
-
-        if self.target_idx >= current_target_idx and self.state.x != 0:
-            current_target_idx = self.target_idx
-
-        # theta_e corrects the heading error
-        theta_e = self.normalize_angle(self.cyaw[current_target_idx] - self.normalize_angle(self.state.theta))
-        # theta_d corrects the cross track error
-        theta_d = np.arctan2(self.k * error_front_axle, self.state.xVel)
-        # Steering control
-        delta = theta_e + theta_d
-
-        delta = delta * self.direction
-
-        return delta, current_target_idx
-
 
     def normalize_angle(self, angle):
         """
@@ -215,79 +191,91 @@ class StanleySteering:
 
         return angle
 
+    def pure_pursuit_steer_control(self, state, trajectory, pind):
+        ind, Lf = trajectory.search_target_index(state, self.k, self.Ka, self.look_ahead)
 
-    def calc_target_index(self):
-        """
-        Compute index in the trajectory list of the target.
-        :param state: (State object)
-        :param cx: [float]
-        :param cy: [float]
-        :return: (int, float)
-        """
-        # Calc front axle position
-        fx = self.state.x + self.L * np.cos(self.normalize_angle(self.state.theta))
-        fy = self.state.y - self.L * np.sin(self.normalize_angle(self.state.theta))
-    
-        # Search nearest point index
-        dx = [fx - icx for icx in self.cx]
-        dy = [fy - icy for icy in self.cy]
-        
+        if pind >= ind:
+            ind = pind
 
-        d = np.hypot(dx, dy)
-        target_idx = np.argmin(d)
-        # Not for the minimum
-        print(d)
-        # Project RMS error onto front axle vector
-        front_axle_vec = [-np.cos(self.normalize_angle(self.state.theta) + np.pi / 2),
-                        -np.sin(self.normalize_angle(self.state.theta) + np.pi / 2)]
-        error_front_axle = np.dot([dx[target_idx], dy[target_idx]], front_axle_vec)
+        print(ind)
+        if ind < len(trajectory.cx):
+            tx = trajectory.cx[ind]
+            ty = trajectory.cy[ind]
+        else:  # toward goal
+            tx = trajectory.cx[-1]
+            ty = trajectory.cy[-1]
+            ind = len(trajectory.cx) - 1
+        print("after",ind)
+        alpha = math.atan2(ty - state.rear_y, tx - state.rear_x) - state.yaw
 
-        return target_idx, error_front_axle
+        delta = math.atan2(2.0 * self.wheel_base * math.sin(alpha) / Lf, 1.0)
 
+        return delta, ind
 
     def get_velocity(self, pose, vel):
-        """Plot an example of Stanley steering control on a cubic spline."""
+        self.state.update(pose.x,pose.y,pose.theta,vel)
+
+        print("LAST+TARGET", self.last_idx, self.target_idx)
+
         desired = Pose()
-        self.state.x = pose.x 
-        self.state.y = pose.y 
-        self.state.theta = pose.theta
-        self.state.xVel = vel
-
-        print("Position of robot", self.state.x,self.state.y)
-
-        print("In get_velcoty loop")
-
-        print("LAST+TARGET", self.last_idx,self.target_idx)
         if self.last_idx > self.target_idx:
             self.is_at_goal = False
-            ai = self.pid_control(self.target_speed, self.state.xVel)
-            di, self.target_idx = self.stanley_control()
-            
-            desired.xVel = self.state.xVel + ai
-            desired.thetaVel = self.state.xVel / self.L * np.tan(di)
 
-            print("STAN: from get_velocity", desired.xVel)
-            self.time += self.dt
+            # Calc control input
+            ai = self.pid_control(self.max_linear_speed, self.state.v)
+            di, self.target_idx = self.pure_pursuit_steer_control(
+                self.state, self.target_course, self.target_idx)
 
-            self.x.append(self.state.x)
-            self.y.append(self.state.y)
-            self.theta.append(self.state.theta)
-            self.v.append(self.state.xVel)
-            self.t.append(self.time)
-
-            # if self.show_animation:  # pragma: no cover
-            #     plt.cla()
-            #     # for stopping simulation with the esc key.
-            #     plt.gcf().canvas.mpl_connect('key_release_event',
-            #             lambda event: [exit(0) if event.key == 'escape' else None])
-            #     plt.plot(self.cx, self.cy, ".r", label="course")
-            #     plt.plot(self.x, self.y, "-b", label="trajectory")
-            #     plt.plot(self.cx[self.target_idx], self.cy[self.target_idx], "xg", label="target")
-            #     plt.axis("equal")
-            #     plt.grid(True)
-            #     plt.title("Speed[km/h]:" + str(self.state.xVel * 3.6)[:4])
-            #     plt.pause(0.001)
+            desired.xVel = self.state.v + ai
+            desired.thetaVel = self.state.v / self.wheel_base * np.tan(di)
         else:
             self.is_at_goal = True
-            
+
         return desired
+
+class TargetCourse:
+
+    def __init__(self, cx, cy):
+        self.cx = cx
+        self.cy = cy
+        self.old_nearest_point_index = None
+
+    def search_target_index(self, state, k, Ka, look_ahead):
+
+        # To speed up nearest point search, doing it at only first time.
+        if self.old_nearest_point_index is None:
+            # search nearest point index
+            dx = [state.rear_x - icx for icx in self.cx]
+            dy = [state.rear_y - icy for icy in self.cy]
+            d = np.hypot(dx, dy)
+            ind = np.argmin(d)
+            self.old_nearest_point_index = ind
+        else:
+            ind = self.old_nearest_point_index
+            distance_this_index = state.calc_distance(self.cx[ind],
+                                                      self.cy[ind])
+            while True:
+                distance_next_index = state.calc_distance(self.cx[ind + 1],
+                                                          self.cy[ind + 1])
+                if distance_this_index < distance_next_index:
+                    break
+                ind = ind + 1 if (ind + 1) < len(self.cx) else ind
+                distance_this_index = distance_next_index
+                if (ind + 1) >= len(self.cx):
+                    break  # not exceed goal
+            self.old_nearest_point_index = ind
+
+        Lf = k * abs(state.v) + look_ahead  # update look ahead distance
+
+        print(state.calc_distance(self.cx[ind], self.cy[ind]))
+        #search look ahead target point index
+        while Lf > state.calc_distance(self.cx[ind], self.cy[ind]):
+            if (ind + 1) >= len(self.cx):
+                break  # not exceed goal
+            ind += 1
+
+        if ind == len(self.cx)-1 and state.calc_distance(self.cx[ind], self.cy[ind]) > 0.3:
+            ind = len(self.cx)-2
+            self.old_nearest_point_index = len(self.cx)-2
+        
+        return ind, Lf
